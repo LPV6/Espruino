144671493d60dcd3e245aa33a368da27946c9822
Gordon Williams <gw@pur3.co.uk>
2021-01-25 14:00:15 +0000

    Boost available RAM for JS on Dickens to more sensible value

--- boards/DICKENS.py
+++ boards/DICKENS.py
@@ -59,7 +59,7 @@ info = {
  'name' : "DICKENS",
  'link' :  [ "" ],
  'espruino_page_link' : '',
- 'variables' : 2500, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 5000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
  'binary_name' : 'espruino_%v_dickens.hex',
  'build' : {
