9100b3e61649cf2c1969eaeec46867736986af38
Gordon Williams <gw@pur3.co.uk>
2021-05-07 10:01:20 +0100

    bump available MTUs

--- boards/DICKENS.py
+++ boards/DICKENS.py
@@ -73,6 +73,8 @@ info = {
 #     'DEFINES += -DNRF_LOG_ENABLED=1 -DNRF_LOG_FILTERS_ENABLED=0',
      'DEFINES += -DCONFIG_NFCT_PINS_AS_GPIOS', # Allow us to use NFC pins as GPIO
      'DEFINES += -DESPR_LSE_ENABLE=1', # Ensure low speed external osc enabled
+     'DEFINES += -DNRF_SDH_BLE_GATT_MAX_MTU_SIZE=131', # 23+x*27 rule as per https://devzone.nordicsemi.com/f/nordic-q-a/44825/ios-mtu-size-why-only-185-bytes
+     'LDFLAGS += -Xlinker --defsym=LD_APP_RAM_BASE=0x2ec0', # set RAM base to match MTU
 #     'DEFINES += -DESPR_REGOUT0_1_8V=1', # Leave REGOUT0 as 1.8v (not 3.3v) - seems to be what original watch firmware did
      'DEFINES += -DESPR_DCDC_ENABLE=1', # Use DC/DC converter
      'DEFINES += -DSPIFLASH_SLEEP_CMD', # SPI flash needs to be explicitly slept and woken up
