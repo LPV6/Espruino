73d9d07caa259ffeeb38e18b1b95e4176dc24f74
Gordon Williams <gw@pur3.co.uk>
2021-05-17 15:29:57 +0100

    Set default console device

--- boards/DICKENS2.py
+++ boards/DICKENS2.py
@@ -19,6 +19,7 @@ info = {
  'boardname' : "DICKENS",
  'link' :  [ "" ],
  'espruino_page_link' : '',
+ 'default_console' : "EV_BLUETOOTH",
  'variables' : 5000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
  'binary_name' : 'espruino_%v_dickens.hex',
