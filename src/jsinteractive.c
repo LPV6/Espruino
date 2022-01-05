182f8423782a607026d8a16799aa946189452399
Richard Blakesley <richard.blakesley@thewandcompany.com>
2022-01-05 10:54:17 +0000

    Reverted change to __FILE__ global

--- src/jsinteractive.c
+++ src/jsinteractive.c
@@ -2260,12 +2260,13 @@ void jsiIdle() {
         jsvKill();
         jshReset();
         jsvInit(0);
-        jsvObjectSetChildAndUnLock(execInfo.root, "__FILE__", jsfVarFromName(filename));
         jsiSemiInit(false); // don't autoload code
         // load the code we specified
         JsVar *code = jsfReadFile(filename,0,0);
-        if (code)
+        if (code) {
+          jsvObjectSetChildAndUnLock(execInfo.root, "__FILE__", jsfVarFromName(filename));
           jsvUnLock2(jspEvaluateVar(code,0,0), code);
+        }
       } else {
         jsiSoftKill();
         jspSoftKill();
